4ef62437961b05eda89e46dc3668d5ec09234cf9
Sergey_Butorin <ButorinSL@gmail.com>
2023-03-31 22:03:51 +0500

    Решили и проверили оператор % для нахождения остатка при делении для 3 задачи ДЗ1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//
+// 4 -> дай
+// -3 -> нет
+// 7 -> нет
+//
+// Задача 4: Напишите программу, которая на вход принимает число (N), а нfа выходе показывает все чётные числа от 1 до N.
+//
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число А->");
+        string valueA; // Объявление переменной типа строка
+        valueA = Console.ReadLine(); // Считываем с консоли строку
+        int numberA = Convert.ToInt32(valueA); // переводим введеное значение в числовой режим
+
+        int ostatok = numberA % 2;
+
+        Console.WriteLine (ostatok);
+    }
+
+}        
+//         if (numberA > numberB)
+//         {
+//         numberMax1 = numberA;
+//         }
+//         else
+//         {
+//         numberMax1 = numberB;
+//         }
+//         if (numberB > numberC)
+//         {
+//         numberMax2 = numberB;
+//         }
+//         else
+//         {
+//         numberMax2 = numberC;
+//         }
+//         if (numberMax1 > numberMax2)
+//         {
+//         numberMAX = numberMax1;
+//         }
+//         else
+//         {
+//         numberMAX = numberMax2;
+//         }
+//         Console.WriteLine("Максимальное число - " + numberMAX);
+//     }
+// }
